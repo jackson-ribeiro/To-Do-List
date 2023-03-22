627c74976e5c0e3c3d6393e744d5fc1d66a2e727
jackson-ribeiro <jacksonquest1@gmail.com>
2023-03-22 01:14:46 -0300

    Estilização do Projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [notification, setNotification] = useState("");
+
+  const handleAddTask = () => {
+    if (newTask !== "") {
+      setTasks([...tasks, { id: Date.now(), text: newTask }]);
+      setNewTask("");
+    }
+  };
+
+  const handleDeleteTask = (id) => {
+    setNotification({
+      id: id,
+      text: "Tem certeza que deseja excluir a tarefa?",
+    });
+  };
+  
+  const handleConfirmDelete = () => {
+    const updatedTasks = tasks.filter((task) => task.id !== notification.id);
+    setTasks(updatedTasks);
+    setNotification("");
+  };
+  
+  const handleCancelDelete = () => {
+    setNotification("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de Tarefas</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Adicione uma tarefa..."
+        />
+        <button onClick={handleAddTask}>Adicionar</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => handleDeleteTask(task.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+      {notification && (
+        <div className="notification">
+          <p>{notification.text}</p>
+          <div className="button-container">
+            <button onClick={handleConfirmDelete}>Sim</button>
+            <button onClick={handleCancelDelete}>Não</button>
+            </div>
+          </div>
+      )}
     </div>
   );
 }
